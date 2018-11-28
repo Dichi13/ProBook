b8cb3a75f000772a168651bebc26279254d3b100
davidtp <13516075@std.stei.itb.ac.id>
2018-11-28 21:54:22 +0700

    rombak ulang book servlet, kali ini lebih penuh harapan!

--- BookServlet/src/BookServlet.java
+++ BookServlet/src/BookServlet.java
@@ -1,16 +1,17 @@
-import java.io.IOException;
+import java.io.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
+import org.json.simple.*;
+import org.json.simple.parser.*;
 
 /**
  * Servlet implementation class BookServlet
  */
+
 @WebServlet("/BookServlet")
 public class BookServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -28,15 +29,20 @@ public class BookServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+		PrintWriter respwriter = response.getWriter();
 		String query = request.getParameter("q");
-		message = query;
+		respwriter.append(query+"<br>");
+		respwriter.append("Served at: ").append(request.getContextPath());
+		
 		try {
-			message = BooksAPI.queryGoogleBooks(jsonFactory, query);
+			JSONObject volumes = BooksAPI.queryGoogleBooks(query);
+			respwriter.append(volumes.toString());
+			
 		} catch (Exception e) {
 			message = e.toString();
 		}
-		response.getWriter().append("Served at: ").append(request.getContextPath()).append("<br>Hello, "+message);
+		
+		respwriter.append(message);
 	}
 
 	/**
