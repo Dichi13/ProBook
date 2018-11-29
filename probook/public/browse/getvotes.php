e7a48d79c7d652e64e97c509b6fbb04dcddd58c1
AbnerAA <abneradhiwijna@gmail.com>
2018-11-30 01:51:50 +0700

    penyelesaian browse dengan angularjs

---
+++ probook/public/browse/getvotes.php
@@ -0,0 +1,17 @@
+<?php 
+    if (isset($_GET['bookid'])) {
+        $db = mysqli_connect("localhost", "root", "", "probookdb");
+        $bookid = $_GET['bookid'];
+        $querySearch = "SELECT count(*) AS votes FROM purchase WHERE bookid = '$bookid'";
+        $result = mysqli_query($db, $querySearch);
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC)
+
+        $count = mysqli_num_rows($result);
+
+        if($count == 1){
+            echo $row['votes'];
+        }else{
+            echo 0;
+        }
+    }
+?>
