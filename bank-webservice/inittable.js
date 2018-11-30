68d271ecbc9d5dcb03c0d1073e632e02c8b4ed9a
dichi <13516063@std.stei.itb.ac.id>
2018-11-30 19:48:36 +0700

    fix transaction to be written in database

--- bank-webservice/inittable.js
+++ bank-webservice/inittable.js
@@ -15,7 +15,7 @@ con.connect(function (err){
         console.log("Table account created");
     });
 
-    var query = "CREATE TABLE IF NOT EXISTS transaction (transactionid INT AUTO_INCREMENT PRIMARY KEY, sender VARCHAR(16) NOT NULL, receiver VARCHAR(16) NOT NULL, AMOUNT INT NOT NULL, t_date DATE NOT NULL)";
+    var query = "CREATE TABLE IF NOT EXISTS transaction (transactionid INT AUTO_INCREMENT PRIMARY KEY, sender VARCHAR(16) NOT NULL, receiver VARCHAR(16) NOT NULL, amount INT NOT NULL, t_date DATE NOT NULL)";
     con.query(query, function (err, result) {
         if (err) throw err;
         console.log("Table transaction created");
